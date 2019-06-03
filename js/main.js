79ee496975174ed66e0c816ad5d572e44d0987a1
Dante <42275387+TJANGEL@users.noreply.github.com>
2019-06-03 14:12:47 -0400

    added rating control functionality

--- js/main.js
+++ js/main.js
@@ -13,6 +13,37 @@ const starsTotal = 5;
 // Run getRatings when DOM loads
 document.addEventListener('DOMContentLoaded', getRatings);
 
+// Form Elements
+const productSelect = document.getElementById('product-select');
+const ratingControl = document.getElementById('rating-control');
+
+// Init product
+let product;
+
+// Product select change
+productSelect.addEventListener('change', e => {
+  product = e.target.value;
+  // enable rating control
+  ratingControl.disabled = false;
+  ratingControl.value = ratings[product];
+});
+
+// Rating Control change
+ratingControl.addEventListener('blur', e => {
+  const rating = e.target.value;
+
+  // Confirm rating <= 5
+  if (rating > 5) {
+    alert('Only Rate 1 - 5');
+    return;
+  }
+
+  // change rating
+  ratings[product] = rating;
+
+  getRatings();
+});
+
 // Get Ratings
 function getRatings() {
   for (let rating in ratings) {
@@ -22,11 +53,13 @@ function getRatings() {
     // Round to nearest 10
     const starPercentageRounded = `${Math.round((starPercentage / 10) * 10)}%`;
 
-    console.log(starPercentageRounded);
-
-    //   Set width of stars-inner to percentage
+    // Set width of stars-inner to percentage
     document.querySelector(
       `.${rating} .stars-inner`
     ).style.width = starPercentageRounded;
+
+    // Add Number Rating
+    document.querySelector(`.${rating} .number-rating`).innerHTML =
+      ratings[rating];
   }
 }
